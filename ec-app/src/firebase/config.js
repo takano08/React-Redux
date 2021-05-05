607a8d9d07323dd2509b3881454f975770bfd554
takano <ri1saxxminny83xx@gmail.com>
2021-05-05 23:38:07 +0900

    firebase用の設定ファイル、cssファイル作成と不要ファイル削除

---
+++ ec-app/src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyA6cX0FA4WYOwQGXZj9-NqEQWkUAtlmoSg",
+    authDomain: "ec-app-49eba.firebaseapp.com",
+    projectId: "ec-app-49eba",
+    storageBucket: "ec-app-49eba.appspot.com",
+    messagingSenderId: "1019758866266",
+    appId: "1:1019758866266:web:0e3f91e1fe34107c79cd63",
+    measurementId: "G-HLERS6WZBG"
+};
