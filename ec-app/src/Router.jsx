d839bd085ea114e7122ea3a99e3cbcd9d8be8a92
takano <ri1saxxminny83xx@gmail.com>
2021-05-13 23:34:27 +0900

    商品を一覧表示できるようにした。

--- ec-app/src/Router.jsx
+++ ec-app/src/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router';
-import {Home, Reset,SignIn, SignUp,ProductEdit} from './templates'
+import { Reset, SignIn, SignUp, ProductEdit, ProductList} from './templates'
 import Auth from "./Auth";
 
 
@@ -11,7 +11,7 @@ const Router = () => {
             <Route exact path={"/signin"} component={SignIn} />
             <Route exact path={"/signin/reset"} component={Reset} />
             <Auth>
-                <Route exact path={"(/)?"} component={Home} />
+                <Route exact path={"(/)?"} component={ProductList} />
                 <Route path={"/product/edit(/:id)?"} component={ProductEdit} />
             </Auth>
         </Switch>
---
+++ ec-app/src/templates/ProductList.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect} from 'react';
+import {ProductCard} from "../components/Products";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchProducts} from "../reducks/products/operations";
+import {getProducts} from "../reducks/products/selectors";
+
+
+const ProductList = () => {
+    const dispatch = useDispatch()
+    const selector = useSelector((state) =>state);
+    const products = getProducts(selector)
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+    },[])
+
+
+    return (
+        <section className="c-section-wrapin">
+            <div className="p-grid__row">
+                {products.length > 0 && (
+                    products.map(product => (
+                        <ProductCard
+                            key={product.id} id={product.id} images={product.images}
+                            price={product.price} name={product.name}
+                        />
+                    ))
+                )}
+            </div>
+        </section>
+    );
+};
+
+export default ProductList;
