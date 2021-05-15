5c9d04720613c51e6e4439e51f916a43654bcf5a
takano <ri1saxxminny83xx@gmail.com>
2021-05-15 23:13:58 +0900

    画像スライダーの作成

--- ec-app/src/templates/ProductDetail.jsx
+++ ec-app/src/templates/ProductDetail.jsx
@@ -1,10 +1,9 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ImageSwiper from "../components/Products/ImageSwiper";
 import {makeStyles} from "@material-ui/styles";
-import {useDispatch, useSelector} from "react-redux";
-import {db, FirebaseTimestamp} from "../firebase";
+import {useSelector} from "react-redux";
+import {db} from "../firebase";
 import {SizeTable} from "../components/Products";
-import {addProductToCart} from "../reducks/users/operations";
 import HTMLReactParser from "html-react-parser";
 
 const useStyles = makeStyles((theme) => ({
@@ -39,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const returnCodeToBr = (text) => {
-    if(text == "") {
+    if(text === "") {
         return text
     }else {
         return HTMLReactParser(text.replace(/\r?\n/g, '<br>'))
@@ -48,7 +47,6 @@ const returnCodeToBr = (text) => {
 
 const ProductDetail = () => {
     const classes = useStyles()
-    const dispatch = useDispatch()
     const selector = useSelector(state => state)
     const path = selector.router.location.pathname
     const id = path.split('/product/')[1]
@@ -62,21 +60,6 @@ const ProductDetail = () => {
         })
     },[])
 
-    const addProduct = useCallback((selectedSize) => {
-        const timestamp = FirebaseTimestamp.now()
-        dispatch(addProductToCart({
-            added_at: timestamp,
-            description: product.description,
-            gender: product.gender,
-            images: product.images,
-            name: product.name,
-            price: product.price,
-            productId: product.id,
-            quantity: 1,
-            size: selectedSize
-        }))
-    }, [product])
-
     return (
         <section className="c-section-wrapin">
             {product && (
@@ -88,7 +71,7 @@ const ProductDetail = () => {
                         <h2 className="u-text__headline">{product.name}</h2>
                         <p className={classes.price}>¥{(product.price).toLocaleString()}</p>
                         <div className="module-spacer--small"/>
-                        <SizeTable addProduct={addProduct} sizes={product.sizes} />
+                        <SizeTable sizes={product.sizes} />
                         <div className="module-spacer--small"/>
                         <p>{returnCodeToBr(product.description)}</p>
                     </div>
